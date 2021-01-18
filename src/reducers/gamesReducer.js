e4ee9a1d9b9f9bd32b92b2b56efd86fd6ee1358d
Henrique <henriqueperoni@hotmail.com>
2021-01-18 17:08:26 +0000

    Fetching other games

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,17 +1,22 @@
 const initState = {
   popular: [],
   newGames: [],
-  upComing: [],
+  upcoming: [],
   searched: [],
 };
 
-const gameReducer = (state = initState, action) => {
+const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state, popular: action.payload.popular };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      };
     default:
       return { ...state };
   }
 };
 
-export default gameReducer;
+export default gamesReducer;
