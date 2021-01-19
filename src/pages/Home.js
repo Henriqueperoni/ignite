144d404c5d1dc0a421b2739a1d97fd8c9f11dc64
Henrique <henriqueperoni@hotmail.com>
2021-01-19 08:18:50 +0000

    Style games cards

---
+++ src/pages/Home.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadGames } from "../actions/gamesAction";
+//Components
+import Game from "../components/Game";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { upcomingGamesURL } from "../api";
+
+const Home = () => {
+  //FETCH GAMES
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
+  return (
+    <GameList>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
+  );
+};
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(390px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
+
+export default Home;
