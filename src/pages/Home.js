a16deb35c64e9ecec82f8eb41ef998cc3e42ee18
Henrique <henriqueperoni@hotmail.com>
2021-01-19 17:38:23 +0000

    React Router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,13 @@ import Game from "../components/Game";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { upcomingGamesURL } from "../api";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  // Get the current location
+  const location = useLocation();
+  const pathId = location.pathname.split("/")[2];
+
   //FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
@@ -20,7 +24,7 @@ const Home = () => {
 
   return (
     <GameList>
-      <GameDetail />
+      {pathId && <GameDetail />}
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map((game) => (
