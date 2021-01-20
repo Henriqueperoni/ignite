da8d39f4778c0f5749964e3ecdc366a930a81025
Henrique <henriqueperoni@hotmail.com>
2021-01-20 14:38:39 +0000

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 // Components and pages
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
+// Styles
 import GlobalStyles from "./components/GlobalStyles";
+// Router
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <GlobalStyles />
       <Route path={["/game/:id", "/"]}>
         <Home />
